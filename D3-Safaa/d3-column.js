318f14ad914ecab0b0b90af974d56a8ae8cd1960
madmure <madelene.mure@gmail.com>
2015-01-21 11:10:27 +0100

    Add line test

---
+++ D3-Safaa/d3-column.js
@@ -0,0 +1,9 @@
+// Set a graph with % per column
+
+	// Link graph to data : categories + gender + values in %
+
+	// Draw the graph according to data
+
+		// Draw the X Y axis
+		// Draw bars for each category with the values
+		// The bars should be fils with 'phone' svg.
